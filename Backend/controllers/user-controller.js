f18cdd7655997769adf72e260291de45723edf74
ArushaK <arusha.kamate007@gmail.com>
2022-04-14 19:31:16 +0530

    Used custom hook

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -50,7 +50,8 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
-    image: "https://unsplash.com/photos/JN0SUcTOig0",
+    image:
+      "https://www.bing.com/images/search?view=detailV2&ccid=W7VOeefH&id=0B7B11010378E6B3FA129D0DB8C224D35A2B1B18&thid=OIP.W7VOeefHm73_-imxG7nN0AHaNL&mediaurl=https%3a%2f%2fwallpapercave.com%2fwp%2fwp5249639.jpg&exph=2200&expw=1237&q=thomas+shelby&simid=608013777082213381&FORM=IRPRST&ck=FA944DF1183DCA4226E595272BCC6E0E&selectedIndex=7&ajaxhist=0&ajaxserp=0",
     password,
     places: [],
   });
--- Frontend/src/user/pages/Users.js
+++ Frontend/src/user/pages/Users.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UsersList from "../components/UsersList";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 function Users(props) {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Arusha",
-      image: "some",
-      places: 7,
-    },
-  ];
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users"
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
   return (
-    <div>
-      <UsersList items={USERS} />
-    </div>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner asOverlay />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
   );
 }
 
